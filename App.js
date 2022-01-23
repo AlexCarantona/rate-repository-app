415ada20fbd4f245780cb89b1f8229cfa74b377b
alexcarantona <info@alejandrocarantonna.es>
2022-01-24 00:25:50 +0100

    #10.12 storing env variables

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import Main from './src/components/Main';
 
+import Constants from 'expo-constants'
+
 import createApolloClient from './src/utils/apolloClient';
 const apolloClient = createApolloClient();
 
 export default function App() {
+  console.log(Constants.manifest)
   return (
   <>
   <NativeRouter>
